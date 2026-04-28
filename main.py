689d939e7507b66efec8b75ce33d2117fa2d03bf
gary-burke <garylewisburke@gmail.com>
2026-04-28 09:17:01 +0200

    Add function to generate unique numbers

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+
+from random import randint
 
 app = FastAPI()
 
 # Mount static files (CSS, JS, images)
-# This lets FastAPI serve everything in your static folder at the '/static' URL
-# That's why your HTML can use: url_for('static', path='css/style.css')
-# Jinja2 resolves that to /static/css/style.css
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
@@ -15,14 +15,24 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="static/templates")
 
 
-@app.get("/")
+@app.get("/")  # Render home page
 async def home(request: Request):
     return templates.TemplateResponse(
         request=request,
         name="index.html"
     )
-"""
-Render home page / index.html
-TemplateResponse API changed — request is now a direct keyword argument
-instead of being passed inside the context dict.
-"""
+
+
+@app.get("/unique-numbers")  # Function to generate unique numbers
+async def unique_nums(
+    unique_amount: int = 0,
+    unique_min: int = 0,
+    unique_max: int = 0
+):
+    nums = []
+    while len(nums) < unique_amount:
+        num = randint(unique_min, unique_max)
+        if num not in nums:
+            nums.append(num)
+
+    return JSONResponse(content={"result": nums})
--- static/js/index.js
+++ static/js/index.js
@@ -2,5 +2,39 @@
 
 // Wait for the DOM to load before executing functions
 document.addEventListener("DOMContentLoaded", function () {
-        
+
+    // Call function '/unique-numbers' from main.py with JS Fetch API
+    document.getElementById("unique-form").addEventListener("submit", async function (e) {
+        e.preventDefault(); // Prevent the page from navigating to the form action URL
+
+        // Get params from form
+        const params = new URLSearchParams({
+            "unique_amount": document.getElementById("unique_amount").value,
+            "unique_min": document.getElementById("unique_min").value,
+            "unique_max": document.getElementById("unique_max").value,
+        });
+
+        // HTML p element where results will be displayed
+        const resultEl = document.getElementById("unique-result");
+
+        // Use try/catch to handle Network-, JSON parse- and HTTP errors
+        try {
+            const response = await fetch(`/unique-numbers?${params}`); // call function from main.py
+
+            // HTTP errors
+            if (!response.ok) {
+                resultEl.textContent = "Something went wrong. Please try again."; // Display friendly error message to user
+                console.error("Bad response:", response.status);
+                return;
+            }
+
+            const data = await response.json(); // Parse data returned from backend to JSON
+            resultEl.textContent = data.result.join(" "); // Display result from function
+
+            // Network/Parse errors
+        } catch (err) {
+            resultEl.textContent = "Network error. Please check your connection."; // Display friendly error message to user
+            console.error("Fetch error:", err);
+        }
+    });
 });
